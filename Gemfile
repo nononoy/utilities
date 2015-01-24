7f8ab96cdb3e68ed527bf65b35522cc8b3241131
Alexandr Muravkin <nedoshel@gmail.com>
2015-01-24 15:16:49 +0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,45 @@
+source 'https://rubygems.org'
+
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails',        '4.2.0'
+
+gem 'mysql2'
+
+gem 'sass-rails',   '~> 5.0'
+gem 'slim-rails',   '~> 3.0.1'
+gem 'uglifier',     '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+
+gem 'jquery-rails'
+gem 'turbolinks'
+
+gem "therubyracer"
+gem "less-rails"
+gem "twitter-bootstrap-rails"
+
+gem "sorcery"
+gem 'simple_form'
+# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+gem 'puma'
+
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+
+
+# gem 'capistrano-rails', group: :development
+
+group :development do
+
+  gem 'letter_opener'
+  # gem 'pry-rails'
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug'
+
+  # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'web-console', '~> 2.0'
+
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+end
+
---
+++ db/schema.rb
@@ -0,0 +1,46 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150124111144) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                           limit: 255,             null: false
+    t.string   "crypted_password",                limit: 255
+    t.string   "salt",                            limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_me_token",               limit: 255
+    t.datetime "remember_me_token_expires_at"
+    t.string   "reset_password_token",            limit: 255
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.string   "activation_state",                limit: 255
+    t.string   "activation_token",                limit: 255
+    t.datetime "activation_token_expires_at"
+    t.integer  "failed_logins_count",             limit: 4,   default: 0
+    t.datetime "lock_expires_at"
+    t.string   "unlock_token",                    limit: 255
+    t.datetime "last_login_at"
+    t.datetime "last_logout_at"
+    t.datetime "last_activity_at"
+    t.string   "last_login_from_ip_address",      limit: 255
+  end
+
+  add_index "users", ["activation_token"], name: "index_users_on_activation_token", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at", using: :btree
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", using: :btree
+
+end
