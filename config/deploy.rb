7fa1a4d133592807fe13fc6d20892b59e4841e99
Alexandr Muravkin <nedoshel@gmail.com>
2015-02-20 09:19:07 +0300

    colorbox

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ namespace :deploy do
     end
   end
 
-  after :publishing, :restart
+  # after :publishing, :restart
 
   # after :restart, :clear_cache do
   #   on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -63,3 +63,4 @@ namespace :deploy do
   # end
 end
 
+after  "deploy:finishing", "deploy:restart"
